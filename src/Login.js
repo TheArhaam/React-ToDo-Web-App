ab6626ac6b599b32ebe9fffa474a9283db8bb71b
Arhaam Patvi <thearhaam@gmail.com>
2020-06-12 20:41:22 +0530

    Initial Commit

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Login() {
+
+    return(
+        <div className="Login">
+            <h1>TO-DO Web Application</h1>
+            <h3>LOGIN</h3>
+            <table>
+
+            </table>
+        </div>
+    );
+
+}
+
+export default Login;
