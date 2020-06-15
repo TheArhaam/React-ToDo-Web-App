7589c64792eb62ef81b16a8fec1971dd8c466e46
Arhaam Patvi <thearhaam@gmail.com>
2020-06-15 15:21:55 +0530

    postToDoList implemented

--- firecloudfunctions/functions/API/todolists.js
+++ firecloudfunctions/functions/API/todolists.js
@@ -1,18 +1,30 @@
 const { db } = require('./admin')
 
+// TO GET TODO LISTS
 exports.getToDoLists = (request, response) => {
     todolists = [
         { 'id': '1', 'name': 'College work' },
         { 'id': '2', 'name': 'House work' },
         { 'id': '3', 'name': 'Workout' },
     ]
+
     return response.json(todolists);
 }
 
+// TO ADD NEW TODOLIST
 exports.postToDoList = (request, response) => {
     var uid = request.body.uid;
     var id = request.body.id;
     var name = request.body.name;
-    db.ref().child('Users').child(uid).child('todolists').child(id).set({ name: name }).catch((err) => { response.send('Error: ' + err) });
+    var addedTime = request.body.addedTime;
+
+    db.ref()
+        .child('Users')
+        .child(uid)
+        .child('todolists')
+        .child(id)
+        .set({ name: name, addedTime: addedTime })
+        .catch((err) => { response.send('Error: ' + err) });
+
     response.send('ToDoList added');
 }
--- firecloudfunctions/functions/API/user.js
+++ firecloudfunctions/functions/API/user.js
@@ -3,7 +3,13 @@ const { db } = require('./admin')
 // FOR REGISTERING A NEW USER
 exports.postNewUser = (request, response) => {
     var uid = String(request.body.uid);
-    db.ref().child('Users').child(uid).set({ todolists: 'null' }).catch((err) => { response.send('Error: ' + err) });
+
+    db.ref()
+        .child('Users')
+        .child(uid)
+        .set({ todolists: 'null' })
+        .catch((err) => { response.send('Error: ' + err) });
+
     response.send('User added');
 }
 
