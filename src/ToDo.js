ad107871942bbd92d5a39f985893f7dd94f901f6
Arhaam Patvi <thearhaam@gmail.com>
2020-06-17 22:44:11 +0530

    Implemented handleTextChange & handleEdit for ToDo

--- src/ToDo.js
+++ src/ToDo.js
@@ -8,11 +8,15 @@ class ToDo extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            text: props.text,
             doneStyle: (props.done === 'true') ? { 'text-decoration': 'line-through' } : {},
             checkedVal: (props.done === 'true') ? true : false,
+            editDisabled: true
         }
         this.handleCheckChange = this.handleCheckChange.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
     }
 
     // TO UPDATE THE DONE VALUE OF TODO
@@ -50,6 +54,47 @@ class ToDo extends Component {
             })
     }
 
+    // TO UPDATE STATE TEXT WHEN TODO IS EDITED
+    handleTextChange = async (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    // TO CHANGE THE EDIT STATE
+    handleEdit = async (e) => {
+        if (this.state.editDisabled) {
+            this.setState({
+                editDisabled: false
+            });
+        }
+        else if (!this.state.editDisabled) {
+            this.setState({
+                editDisabled: true
+            })
+
+            const authToken = localStorage.getItem('AuthToken');
+            const uid = localStorage.getItem('userID');
+
+            const toDo = {
+                uid: uid,
+                listid: this.props.listid,
+                todoid: this.props.id,
+                text: this.state.text
+            }
+
+            axios.defaults.headers.common = { Authorization: `Bearer ${authToken}` };
+            await axios.post('/todos/edit', toDo)
+                .then((response) => {
+                    alert(response.data)
+                    // window.location.reload();
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
+    }
+
     // TO DELETE TODO
     handleDelete = async () => {
         const authToken = localStorage.getItem('AuthToken');
@@ -82,14 +127,15 @@ class ToDo extends Component {
                             <tr>
                                 <label htmlFor={this.props.id}>
                                     <td>
-                                        {this.props.text}
+                                        <input type="text" value={this.state.text} disabled={this.state.editDisabled} onChange={this.handleTextChange} style={this.state.doneStyle} />
+
                                     </td>
                                 </label>
                                 <td>
                                     <button onClick={this.handleDelete}>DELETE</button>
                                 </td>
                                 <td>
-                                    <button>EDIT</button>
+                                    <button onClick={this.handleEdit}>EDIT</button>
                                 </td>
                             </tr>
                         </table>
